39cdd638e36f434dcb92a03bfed2a5f0ef9507f4
Matt McFarland <matmcfarland@gmail.com>
2014-02-03 12:04:01 -0600

    add custom error name for specific error modules

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ for(var m in modules) {
   errors[m] = {};
 
   for(var error in modules[m]) {
-    expose(errors[m], error, modules[m][error]);
+    expose(m, error, modules[m][error]);
   }
 }
 
 /**
- * Helper function for exposing errors
+ * Helper function for exposing errors,
+ * also edits the stack trace to remove itself.
  *
  * @param {Object} anchor
  * @param {String} name
@@ -31,10 +32,25 @@ for(var m in modules) {
  */
 
 function expose(anchor, name, message) {
-  Object.defineProperty(anchor, name, {
+  Object.defineProperty(errors[anchor], name, {
     enumerable: true,
     get: function() {
-      return new Error(message);
+      var err = new Error();
+
+      err.name = capitalize(anchor) + 'Error';
+      err.message = message;
+      return err;
     }
   });
+}
+
+/**
+ * Capitalize the first letter of `str`
+ *
+ * @param {String} str
+ * @return {String}
+ */
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
